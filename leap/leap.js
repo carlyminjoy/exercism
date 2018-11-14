109ae29990019a3341ffb9aa623d5550d00dc80a
Carly Minjoy <cminjoy@myune.edu.au>
2018-11-14 20:17:54 +1000

    Update gitignore & leap refactor

--- leap/leap.js
+++ leap/leap.js
@@ -1,3 +1,6 @@
-const divisibleBy = (year, x) => year % x === 0;
+const divisibleBy = x => year => year % x === 0;
+const divisibleBy4 = divisibleBy(4);
+const divisibleBy100 = divisibleBy(100);
+const divisibleBy400 = divisibleBy(400);
 
-export const isLeap = year => divisibleBy(year, 4) && (!divisibleBy(year, 100) || divisibleBy(year, 400));
+export const isLeap = year => divisibleBy4(year) && (!divisibleBy100(year) || divisibleBy400(year));
